de5505c80f69308474923754064a66531df8dae0
Boi Octave <boiOctave@gmail.com>
2024-02-17 23:20:14 +0100

    java init

---
+++ src/feb17/Print.java
@@ -0,0 +1,12 @@
+package feb17;
+
+public class Print {
+	
+	public  void poo(int k) {
+		
+	
+		System.out.println( k);
+		
+	}
+
+}
---
+++ src/feb17/feb17.java
@@ -0,0 +1,26 @@
+package feb17;
+
+public class feb17 {
+
+	public static  void main(String arg[]) {
+		Student s1 = new Student();
+		
+		Print p = new Print(); 
+		
+		
+		s1.setX(1);
+		s1.setY(20);
+		
+		
+		int a = s1.getX();
+		int b = s1.getY();
+		
+		p.poo(a);
+		p.poo(b);
+		
+		
+		
+		
+		
+	}
+}
